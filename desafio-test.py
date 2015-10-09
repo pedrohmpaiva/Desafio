3d41ac8a1da0a43a47b1f511e12a4db9b9d8fd13
Nitai Charan Alvares Pereira <nitaicharan@gmail.com>
2015-10-09 09:34:52 -0300

    Programa
    Este programa tem a função de controlar  a venda de ingressos de uma
    sala de cinema com assentos reservados.

--- desafio-test.py
+++ desafio-test.py
@@ -1,6 +1,39 @@
-print(' testando o git')
-print('1')
-print('2')
-print('3')
-print('4')
-print('5')
+##Começar com while pois ao final da venda o usuário deverar escolher se vai querer continuar ou sair (Opção 6)
+##Como não sabemos o número de vezes usuários irão comprar ou cancelar as vendas, iremos usar "while" e não "for"!
+
+while True 
+	##Visualização primaria do programa
+	
+	print ('---------------------------')
+	print ('  SALA DE CINEMA – CINEPÊ  ')
+	print ('---------------------------')
+	print ('1- EXIBIR MAPA DA SALA')
+	print ('2- DETALHAR INGRESSO')
+	print ('3- VENDER INGRESSO')
+	print ('4- CANCELAR VENDA')
+	print ('5- INICIAR NOVA SESSÃO')
+	print ('6- SAIR')
+	opçao = input( print ('OPÇÃO DESEJADA:'))
+
+	## Opção ('1- EXIBIR MAPA DA SALA')
+
+if opçao == '1':
+
+	print ('---------------------------')
+	print ('  SALA DE CINEMA – CINEPÊ  ')
+	print ('---------------------------')
+	print ('        MAPA DA SALA       ')
+	print ('     A B C D E F G H I J   ')
+	print ('  01 - - - - - - - x - -   ')
+	print ('  02 x x - - x - - - x -   ')
+	print ('  03 - x x - x - x x - -   ')
+	print ('  04 - - - - x - - x - -   ')
+	print ('  05 - x - x - - - x - x   ')
+	print ('  06 - x - - x x - x x -   ')
+	print ('  07 - x - - - - - x x -   ')
+	print ('  08 x x x - x - - - - x   ')
+	print ('  09 - x x x x - - x - x   ')
+	print ('  10 x x x x x x - x - -   ')
+	print (PRESSIONE QUALQUER TECLA PARA VOLTAR...
+		
+break
